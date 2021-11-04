edd00ab4b55e98698d3632c7caa128ff2e49fe8d
Matt Heins <matthew.heins@nordicsemi.no>
2021-11-04 11:12:26 -0500

    Added separate builds for devices and settings with pre-compiled hex files.

--- src/main.c
+++ src/main.c
@@ -70,13 +70,9 @@ struct uart_data_t {
 static K_FIFO_DEFINE(fifo_uart_tx_data);
 static K_FIFO_DEFINE(fifo_uart_rx_data);
 
-static struct bt_le_ext_adv *adv;
-static struct k_work start_advertising_worker;
-
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
 	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
-	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_NUS_VAL)
 };
 
 static const struct bt_data sd[] = {
@@ -325,30 +321,17 @@ static int uart_init(void)
 
 static void connected(struct bt_conn *conn, uint8_t err)
 {
-	char addr[BT_ADDR_LE_STR_LEN];
-	struct bt_conn_info info;
+	// char addr[BT_ADDR_LE_STR_LEN];
+	// struct bt_conn_info info;
 
 	if (err) {
 		LOG_ERR("Connection failed (err %u)", err);
 		return;
 	}
 
-	
-	//LOG_INF("Connected %s", log_strdup(addr));
-
 	current_conn = bt_conn_ref(conn);
 
-	err = bt_conn_get_info(conn, &info);
-
-	if (err) {
-		printk("Failed to get connection info\n");
-	} else {
-		const struct bt_conn_le_phy_info *phy_info;
-		phy_info = info.le.phy;
-		bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-		LOG_INF("Connected: %s, tx_phy %u, rx_phy %u\n",
-		       log_strdup(addr), phy_info->tx_phy, phy_info->rx_phy);
-	}
+	LOG_INF("Connected");
 
 	dk_set_led_on(CON_STATUS_LED);
 }
@@ -371,8 +354,6 @@ static void disconnected(struct bt_conn *conn, uint8_t reason)
 		current_conn = NULL;
 		dk_set_led_off(CON_STATUS_LED);
 	}
-
-	k_work_submit(&start_advertising_worker);
 }
 
 #ifdef CONFIG_BT_NUS_SECURITY_ENABLED
@@ -588,48 +569,6 @@ static void configure_gpio(void)
 	}
 }
 
-static int create_advertising_coded(void)
-{
-	int err;
-	struct bt_le_adv_param param =
-		BT_LE_ADV_PARAM_INIT(BT_LE_ADV_OPT_CONNECTABLE |
-				     BT_LE_ADV_OPT_EXT_ADV |
-				     BT_LE_ADV_OPT_CODED,
-				     BT_GAP_ADV_FAST_INT_MIN_2,
-				     BT_GAP_ADV_FAST_INT_MAX_2,
-				     NULL);
-
-	err = bt_le_ext_adv_create(&param, NULL, &adv);
-	if (err) {
-		LOG_ERR("Failed to create advertiser set (%d)\n", err);
-		return err;
-	}
-
-	LOG_INF("Created advertiser");
-
-	err = bt_le_ext_adv_set_data(adv, ad, ARRAY_SIZE(ad), NULL, 0);
-	if (err) {
-		LOG_ERR("Failed to set advertising data (%d)\n", err);
-		return err;
-	}
-
-	return 0;
-}
-
-static void start_advertising_coded(struct k_work *item)
-{
-	int err;
-
-	err = bt_le_ext_adv_start(adv, NULL);
-	if (err) {
-		LOG_INF("Failed to start advertising set (%d)\n", err);
-		return;
-	}
-
-	LOG_INF("Advertiser started\n");
-}
-
-
 void main(void)
 {
 	int blink_status = 0;
@@ -667,22 +606,19 @@ void main(void)
 		return;
 	}
 
-	k_work_init(&start_advertising_worker, start_advertising_coded);
+	struct bt_le_adv_param *adv_param =
+		BT_LE_ADV_PARAM(BT_LE_ADV_OPT_CONNECTABLE |
+				BT_LE_ADV_OPT_NO_2M,
+				BT_GAP_ADV_FAST_INT_MIN_2,
+				BT_GAP_ADV_FAST_INT_MAX_2,
+				NULL);
 
-	err = create_advertising_coded();
+	err = bt_le_adv_start(adv_param, ad, ARRAY_SIZE(ad), sd,
+			      ARRAY_SIZE(sd));
 	if (err) {
-		LOG_ERR("Advertising failed to create (err %d)\n", err);
+		LOG_ERR("Advertising failed to start (err %d)", err);
 		return;
 	}
-
-	k_work_submit(&start_advertising_worker);
-
-	// err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), sd,
-	// 		      ARRAY_SIZE(sd));
-	// if (err) {
-	// 	LOG_ERR("Advertising failed to start (err %d)", err);
-	// 	return;
-	// }
 
 	for (;;) {
 		if (current_conn == NULL){
